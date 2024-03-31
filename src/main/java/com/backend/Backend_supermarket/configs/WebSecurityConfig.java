c09075cdfa3639f858eaa8461aac44641c03e217
Giang03 <gianglvci@gmail.com>
2024-03-31 17:08:46 +0700

    Cài đặt Jwt cho dự án

--- src/main/java/com/backend/Backend_supermarket/configs/WebSecurityConfig.java
+++ src/main/java/com/backend/Backend_supermarket/configs/WebSecurityConfig.java
@@ -1,22 +1,42 @@
 package com.backend.Backend_supermarket.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.backend.Backend_supermarket.filters.JwtTokenFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
     
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Value("${api.prefix}")
+    private String apiPrefix;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(request ->{
-                request.anyRequest().permitAll();
+                request
+                    .requestMatchers(
+                        HttpMethod.GET,
+                        String.format("%s/products**", apiPrefix),
+                        String.format("%s/products", apiPrefix)
+                    ).permitAll()
+                    .anyRequest().authenticated();
             });
         return http.build();
     }
