86bee5e14754a779ec710c9fba7d3fcdf5f02372
Xuân Nghị Phạm <121102789+XuanNghi19@users.noreply.github.com>
2024-05-06 19:51:17 +0700

    Update CorsFilter.java

--- src/main/java/com/backend/Backend_supermarket/CorsFilter.java
+++ src/main/java/com/backend/Backend_supermarket/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "10000");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
         if ("OPTIONS".equals(request.getMethod())) {
